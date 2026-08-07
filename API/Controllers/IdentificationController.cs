ef4810f22c33b9d9f51a4e2da36bbdfcef28eae5
Edison Herrera <erherrera.alvarez@gmail.com>
2026-08-07 17:52:08 -0500

    test

--- API/Controllers/IdentificationController.cs
+++ API/Controllers/IdentificationController.cs
@@ -66,7 +66,7 @@ namespace API.Controllers
             }
         }
         /// <summary>
-        /// Test 
+        /// Test get api
         /// </summary>
         /// <returns></returns>
         [HttpGet(Name = "GetTestForecast")]
